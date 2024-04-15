816307fe55cf915b06d6733b1b3acde415a6576d
pawciut <paweltruong@o2.pl>
2024-04-15 18:04:38 +0200

    complete game loop

--- TSOTIH/Assets/Scripts/Controllers/SceneEpilogueController.cs
+++ TSOTIH/Assets/Scripts/Controllers/SceneEpilogueController.cs
@@ -1,12 +1,23 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneEpilogueController : SceneLogicController
 {
+    [SerializeField, Required] SceneLogicController MainMenuScene;
     public override void ResetSceneLogic()
     {
         base.ResetSceneLogic();
+    }
 
+    public override void GoRight()
+    {
+        base.GoRight();
+
+        MainMenuScene.ResetSceneLogic();
+        MainMenuScene.gameObject.SetActive(true);
+        gameObject.SetActive(false);
+        ResetSceneLogic(); 
     }
 }
